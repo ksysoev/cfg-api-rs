73e4db29bd5d006c8f54cf9bf3ffa713bae57648
Кирилл Сысоев <k.sysoev@me.com>
2023-09-19 23:01:18 +0800

    implements health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init();
+    let server = cfg_api::CfgServer::new();
+    server.run().await
 }
